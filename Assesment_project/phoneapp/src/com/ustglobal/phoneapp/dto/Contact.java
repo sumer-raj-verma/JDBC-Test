4ca4aef2e04ed115f45fde6e97c67fe3bf8fb2e7
sumer-raj-verma <56624114+sumer-raj-verma@users.noreply.github.com>
2019-11-25 14:29:35 +0530

    Add files via upload

---
+++ Assesment_project/phoneapp/src/com/ustglobal/phoneapp/dto/Contact.java
@@ -0,0 +1,26 @@
+package com.ustglobal.phoneapp.dto;
+
+public class Contact {
+	private String name;
+	private long number;
+	private String groups;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getNumber() {
+		return number;
+	}
+	public void setNumber(long number) {
+		this.number = number;
+	}
+	public String getGroups() {
+		return groups;
+	}
+	public void setGroups(String groups) {
+		this.groups = groups;
+	}
+
+}
